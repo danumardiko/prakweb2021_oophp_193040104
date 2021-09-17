7aeee0b56a2861315e28a438745a8d348f608648
danumardiko <danumardiko123@gmail.com>
2021-09-17 18:49:28 +0700

    menyelesaikan class dan object

---
+++ pertemuan2/coba.php
@@ -0,0 +1,24 @@
+
+
+
+<?php
+/*
+Danu Mardiko
+193040104
+https://github.com/danumardiko/prakweb2021_oophp_193040104
+Pertemuan 2  17 september 2021
+Mengerjakan tugas 1
+*/ 
+?>
+<?php
+
+class Coba {
+
+    $a = new Coba();
+    $b = new Coba();
+    $c = new Coba();
+}
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
