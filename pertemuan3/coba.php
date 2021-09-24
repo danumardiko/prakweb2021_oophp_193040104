8b59ff6c1062f05199c58946843aa3a541358ba6
danumardiko <danumardiko123@gmail.com>
2021-09-24 09:40:50 +0700

    meyelesaikan vidio 6 inheritance

---
+++ pertemuan3/coba.php
@@ -0,0 +1,24 @@
+
+
+
+<?php
+/*
+Danu Mardiko
+193040104
+https://github.com/danumardiko/prakweb2021_oophp_193040104
+Pertemuan 3  24 september 2021
+Mengerjakan tugas 1
+*/ 
+?>
+<?php
+
+class Coba {
+
+    $a = new Coba();
+    $b = new Coba();
+    $c = new Coba();
+}
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
